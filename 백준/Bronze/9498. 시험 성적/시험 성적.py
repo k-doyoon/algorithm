e56ac29063027942611ecac8fdfdc65504309fb3
k-doyoon <kwondonic@gmail.com>
2025-01-25 15:59:26 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a=int(input())
+if 89 <a and a<=100 :
+    print("A")
+elif 79<a and a<90:
+    print("B")
+elif 80>a and a>69:
+    print("C")
+elif 70>a and a>59:
+    print("D")
+else:
+    print("F")
